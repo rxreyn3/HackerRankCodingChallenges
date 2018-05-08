703e380c53a99c02c29c85232515aa9de8eafc2b
thevoid\deadpool <ryan.reynolds@thevoid.com>
2018-05-08 09:11:05 -0600

    Updated scala.

--- ScalaCodingChallenges/src/Fibonacci.scala
+++ ScalaCodingChallenges/src/Fibonacci.scala
@@ -2,17 +2,17 @@
 
 object Fibonacci {
   def fibonacci(x: Int): Int = {
-    if(x <= 1)
-    {
+    if (x <= 1) {
       return x;
     }
-    return fibonacci(x -1) + fibonacci(x - 2);
-
+    val left = fibonacci(x - 1);
+    val right = fibonacci(x - 2);
+    val total = left + right;
+    println("Left: " + left + " Right: " + right + " Total: " + total);
+    return fibonacci(x - 1) + fibonacci(x - 2);
   }
 
   def main(args: Array[String]) {
-    /** This will handle the input and output**/
     println(fibonacci(scala.io.StdIn.readInt()))
-
   }
 }
